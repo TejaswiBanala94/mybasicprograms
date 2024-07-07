c6afbeb36e2625994e64123ff65995f1d3139fdd
Tejaswi <banalatejaswi555@gmail.com>
2024-07-07 12:35:58 -0500

    Access specifier for constructor1 p2

---
+++ Access_Specifier_Constructor1.java
@@ -0,0 +1,14 @@
+package p2;
+
+import p1.Access_Specifier_Constructor;
+
+public class Access_Specifier_Constructor1 
+{
+
+	public static void main(String[] args)
+	{
+		Access_Specifier_Constructor a1 = new Access_Specifier_Constructor();
+
+	}
+
+}
