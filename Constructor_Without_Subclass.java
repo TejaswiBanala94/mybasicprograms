74ba256a2feabf5fcb2f33a1b481b37c424de2e8
Tejaswi <banalatejaswi555@gmail.com>
2024-07-07 12:49:05 -0500

    Access specifier for constructor not subclass packge2

---
+++ Constructor_Without_Subclass.java
@@ -0,0 +1,15 @@
+package package2_Con;
+
+import package1_Con.Constructor_Without_Subcls;
+
+public class Constructor_Without_Subclass 
+{
+	public static void main(String[] args) 
+	{
+		 new Constructor_Without_Subcls();
+//		 new Constructor_Without_Subcls(20);
+//		 new Constructor_Without_Subcls(38.56);
+//		 new Constructor_Without_Subcls("Tejaswi");
+	}
+
+}
