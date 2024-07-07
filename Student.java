22a73ddae7e0bf7b86e81cb95f9e59657c4c0062
Tejaswi <banalatejaswi555@gmail.com>
2024-07-07 12:14:50 -0500

    Access specifier within the package

---
+++ Student.java
@@ -0,0 +1,14 @@
+package accessspecifier;
+
+public class Student {
+
+	public static void main(String[] args)
+	{
+		Teacher t1 = new Teacher();
+		t1.add();
+		t1.sub();
+		t1.div();
+
+	}
+
+}
---
+++ Teacher.java
@@ -0,0 +1,22 @@
+package accessspecifier;
+
+public class Teacher 
+{
+	public void add()
+	{
+		System.out.println("add");
+	}
+	protected void sub()
+	{
+		System.out.println("sub");
+	}
+	private void mul()
+	{
+		System.out.println("mul");
+	}
+	void div()
+	{
+		System.out.println("div");
+	}
+	
+}
