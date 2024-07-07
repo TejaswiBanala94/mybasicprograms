e555f626831a5f0526a6b2b2d2b6f4be98f7ac56
Tejaswi <banalatejaswi555@gmail.com>
2024-07-07 12:45:55 -0500

    Access specifier outside the class not becoming the subclass

---
+++ Class2_for_outide_notsubclass.java
@@ -0,0 +1,22 @@
+package package2;
+
+public class Class2_for_outide_notsubclass 
+{
+	public void add()
+	{
+		System.out.println("add");
+	}
+	protected void sub()
+	{
+		System.out.println("sub");
+	}
+	private void mul()
+	{
+		System.out.println("mul");
+	}
+	void div()
+	{
+		System.out.println("div");
+	}
+
+}
