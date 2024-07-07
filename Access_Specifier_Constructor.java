0aba4bc7a2fb38acf31ef752a828a0ce06581647
Tejaswi <banalatejaswi555@gmail.com>
2024-07-07 12:33:21 -0500

    Access specifiers for constructors p1

---
+++ Access_Specifier_Constructor.java
@@ -0,0 +1,11 @@
+package p1;
+
+public class Access_Specifier_Constructor
+{
+	
+	 public Access_Specifier_Constructor()
+	{
+		System.out.println("A Constructor outside the package without being a subclass");
+	}
+
+}
