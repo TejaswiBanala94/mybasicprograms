729ca5eef9edc668e15d6dc5f08689f7ab70035e
Tejaswi <banalatejaswi555@gmail.com>
2024-07-07 12:52:12 -0500

    Access specifier for class outside package with subclass

---
+++ Cls1_ousidepackge_subclass.java
@@ -0,0 +1,15 @@
+package pckge1;
+
+import pckge2.Cls2_outsidepackage_subclass;
+
+public class Cls1_ousidepackge_subclass extends Cls2_outsidepackage_subclass 
+{
+
+	public static void main(String[] args)
+	{
+		Cls1_ousidepackge_subclass c1 = new Cls1_ousidepackge_subclass();
+		c1.add();
+		c1.sub();
+	}
+
+}
