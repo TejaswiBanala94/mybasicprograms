9817a72ec7a2be36bada51e3d8b1db87219edb15
Tejaswi <banalatejaswi555@gmail.com>
2024-07-07 12:39:44 -0500

    Access specifier for package1 outside the package not becoming the subclass

---
+++ Class1_for_outside_Package_not_Subclass.java
@@ -0,0 +1,15 @@
+package package1;
+
+import package2.Class2_for_outide_notsubclass;
+
+public class Class1_for_outside_Package_not_Subclass 
+{
+
+	public static void main(String[] args) 
+	{
+		Class2_for_outide_notsubclass c2 = new Class2_for_outide_notsubclass();
+		c2.add();	
+
+	}
+
+}
