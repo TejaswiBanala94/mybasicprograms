a7e50d6c86c88aa2e0e7bfdee69d15c08fd6e051
Tejaswi <banalatejaswi555@gmail.com>
2024-07-07 12:54:08 -0500

    Access specifier for Class2 outside the package with subclass

---
+++ Cls2_outsidepackage_subclass.java
@@ -0,0 +1,22 @@
+package pckge2;
+
+public class Cls2_outsidepackage_subclass 
+{
+	public void add()
+	{
+		System.out.println("add");
+	}
+	protected void sub()
+	{
+		System.out.println("sub");
+	}
+	private void mul()
+	{
+		System.out.println("mul");
+	}
+	void div()
+	{
+		System.out.println("div");
+	}
+
+}
